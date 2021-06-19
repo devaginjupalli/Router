b05ef3078063dd70e8ca95e6cfd7a9bfadf6feca
devanand <ggdevanand@gmail.com>
2021-06-19 03:49:31 +0000

    router

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const About = () => (
+  <div className="contact-container">
+    <img
+      alt="Contact"
+      className="image"
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+    />
+    <h1 className="heading">About</h1>
+  </div>
+)
+
+export default About
